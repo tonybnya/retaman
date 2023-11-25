e9a50f85d3d4d4805d8c94fd16744e9b3f1e38ad
Tony <nya.tony2010@gmail.com>
2023-11-25 01:40:56 +0100

    feat(app.js): add view engine setup and configuration and dependencies needed for the app module to run

--- app.js
+++ app.js
@@ -4,4 +4,15 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const redis = require('redis');
 
+// Initialize the app
 const app = express();
+
+// View engine setup
+app.set('views', path.join(__dirname, (views));
+app.set('view engine', 'ejs');
+
+// Configuration and dependencies needed for the module to run
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
