3783f3a29258a71d341243618f2f330d430dab61
Tony <nya.tony2010@gmail.com>
2023-11-25 01:31:53 +0100

    feat(app.js): add the main file 'app.js'

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const redis = require('redis');
+
+const app = express();
