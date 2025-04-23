c9d64094568f6e943b38011d36c4150df9b30c95
tonybnya <nya.tony2010@gmail.com>
2025-04-23 18:34:12 +0100

    feat: set up the Node.js Express server

--- app.js
+++ app.js
@@ -4,4 +4,22 @@ const logger = require("morgan");
 const redis = require("redis");
 const path = require("path");
 
+// init the main app
 const app = express();
+// set up the server port
+const port = 3000;
+
+// set up the views
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// set up
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(port);
+console.log(`Server started on port ${port}...`);
+
+module.exports = app;
